44a0e8f46f51fda7ac1f0f18830f83af39cfa0dd
ivan <ivanlsalvadori@gmail.com>
2016-07-28 20:47:00 -0300

    Small fixes - Link validation.

--- src/main/java/br/ufsc/inf/lapesd/linkedator/jersey/LinkedadorWriterInterceptor.java
+++ src/main/java/br/ufsc/inf/lapesd/linkedator/jersey/LinkedadorWriterInterceptor.java
@@ -1,22 +1,43 @@
 package br.ufsc.inf.lapesd.linkedator.jersey;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 import javax.ws.rs.ext.WriterInterceptor;
 import javax.ws.rs.ext.WriterInterceptorContext;
 
 @Provider
-public class LinkedadorWriterInterceptor implements WriterInterceptor {
+public class LinkedadorWriterInterceptor implements WriterInterceptor, ContainerRequestFilter {
     
     private LinkedadorApi linkedatorApi = new LinkedadorApi();
 
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        MultivaluedMap<String, String> queryParameters = requestContext.getUriInfo().getQueryParameters();
+        List<String> linkedatorOptions = queryParameters.get("linkedatorOptions");
+        requestContext.setProperty("linkedatorOptions", linkedatorOptions);
+    }
+    
     @Override
     public void aroundWriteTo(WriterInterceptorContext context) throws IOException, WebApplicationException {
         Object entity = context.getEntity();
-        String representationWithLinks = linkedatorApi.createLinks(entity.toString());
-        context.setEntity(representationWithLinks);
+        List<String> linkedatorOptions = (List<String>) context.getProperty("linkedatorOptions");
+        if(linkedatorOptions != null && linkedatorOptions.contains("linkVerify")){
+            return;
+        }
+                
+        if(entity != null){
+            String representationWithLinks = linkedatorApi.createLinks(entity.toString());
+            context.setEntity(representationWithLinks);
+        }
         context.proceed();
+        
     }
+    
 }
---
+++ src/main/java/br/ufsc/inf/lapesd/linkedator/jersey/SemanticClass.java
@@ -0,0 +1,13 @@
+package br.ufsc.inf.lapesd.linkedator.jersey;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SemanticClass {
+
+    String[] id() default "";
+
+}
