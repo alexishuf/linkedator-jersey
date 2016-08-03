9de3221e054be7a579bd07b604d2a5b1b4343526
ivan <ivanlsalvadori@gmail.com>
2016-08-02 21:39:22 -0300

    Time stamp.

--- src/main/java/br/ufsc/inf/lapesd/linkedator/jersey/LinkedadorWriterInterceptor.java
+++ src/main/java/br/ufsc/inf/lapesd/linkedator/jersey/LinkedadorWriterInterceptor.java
@@ -2,6 +2,7 @@ package br.ufsc.inf.lapesd.linkedator.jersey;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -11,33 +12,58 @@ import javax.ws.rs.ext.Provider;
 import javax.ws.rs.ext.WriterInterceptor;
 import javax.ws.rs.ext.WriterInterceptorContext;
 
+import com.google.common.base.Stopwatch;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 @Provider
 public class LinkedadorWriterInterceptor implements WriterInterceptor, ContainerRequestFilter {
-    
+
     private LinkedadorApi linkedatorApi = new LinkedadorApi();
 
-    
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         MultivaluedMap<String, String> queryParameters = requestContext.getUriInfo().getQueryParameters();
         List<String> linkedatorOptions = queryParameters.get("linkedatorOptions");
         requestContext.setProperty("linkedatorOptions", linkedatorOptions);
     }
-    
+
     @Override
     public void aroundWriteTo(WriterInterceptorContext context) throws IOException, WebApplicationException {
+        Stopwatch time = Stopwatch.createStarted();
         Object entity = context.getEntity();
         List<String> linkedatorOptions = (List<String>) context.getProperty("linkedatorOptions");
-        if(linkedatorOptions != null && linkedatorOptions.contains("linkVerify")){
+        if (linkedatorOptions != null && linkedatorOptions.contains("linkVerify")) {
             return;
         }
-                
-        if(entity != null){
+
+        if (entity != null) {
             String representationWithLinks = linkedatorApi.createLinks(entity.toString());
+
+            JsonElement parseRepresentation = timeStamp(time, representationWithLinks);
+            
+            representationWithLinks = parseRepresentation.toString();
+
             context.setEntity(representationWithLinks);
         }
         context.proceed();
-        
+
     }
-    
+
+    private JsonElement timeStamp(Stopwatch time, String representationWithLinks) {
+        long elapsed = time.elapsed(TimeUnit.MICROSECONDS);
+        double processingTime = elapsed/1000.0;
+        
+        JsonElement parseRepresentation = new JsonParser().parse(representationWithLinks);
+        if (parseRepresentation.isJsonObject()) {
+            parseRepresentation.getAsJsonObject().addProperty("linkedatorJerseyTime", processingTime);
+        } else if (parseRepresentation.isJsonArray()) {
+            JsonObject timeObject = new JsonObject();
+            timeObject.addProperty("linkedatorJerseyTime", processingTime);
+            parseRepresentation.getAsJsonArray().add(timeObject);
+        }
+        return parseRepresentation;
+    }
+
 }
